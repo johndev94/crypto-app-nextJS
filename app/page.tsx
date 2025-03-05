20d2ae30394816caa2fc49e4fd8b4753f1b26d69
John Devaney <johndevaney1@hotmail.com>
2025-03-05 15:26:09 +0000

    Have price cards with data somewhat functional

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,24 @@
 import { useTheme } from "next-themes";
 import { Header } from "./Components/Header";
 import { useEffect, useState } from "react";
+import { PriceCards } from "./Components/price-cards";
 
 export default function Home() {
   const { theme } = useTheme();
-  const [bgColor, setBgColor] = useState(theme === "dark" ? "bg-black" : "bg-slate-100");
+  const [bgColor, setBgColor] = useState(
+    theme === "dark" ? "bg-black" : "bg-slate-100"
+  );
 
   useEffect(() => {
     setBgColor(theme === "dark" ? "bg-black" : "bg-slate-100");
   }, [theme]);
 
   return (
-    <div className={`poppins ${bgColor} p-2 min-h-screen transition-colors duration-300`}>
+    <div
+      className={`poppins ${bgColor} p-2 min-h-screen transition-colors duration-300`}
+    >
       <Header />
+      <PriceCards />
     </div>
   );
 }
